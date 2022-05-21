d9975c8e10e3e881586e1c68b7e31255ef66c238
Andres <abetancurthb@unal.edu.co>
2022-05-21 16:44:45 -0500

    ProyectoPerspectiva

---
+++ content/docs/Perspective Superliminal/funcion PixelRatio.md
@@ -0,0 +1,13 @@
+## El mundo y el pixel
+
+{{< p5-iframe  sketch="/showcase/sketches/MundoEnPixelP1.js" width="425" height="425" >}}
+
+### desnidad del mundo
+
+{{< p5-iframe  sketch="/showcase/sketches/MundoEnPixelP2.js" width="425" height="425" >}}
+
+
+### alejandonos
+
+{{< p5-iframe  sketch="/showcase/sketches/MundoEnPixelP3.js" width="425" height="425" >}}
+
---
+++ content/sketches/MundoEnPixelP2.js
@@ -0,0 +1,48 @@
+let z;
+let size;
+let rotation;
+let slider;
+let sliderD;
+
+function setup() {
+
+  sliderZ = createSlider(-100,0, 0);
+  sliderZ.position(10, 10);
+  /*
+  sliderS = createSlider(200,600, 400,10);
+  sliderS.position(10, 40);
+  */
+  sliderR = createSlider(0,1.55,0,0.05);
+  sliderR.position(10, 350);
+
+  z = 0;
+  size = 400;
+
+  createCanvas(400, 400,WEBGL);
+  img = loadImage("/tierra.jpg");
+}
+
+function draw() {
+  
+  background(220);
+  z = sliderZ.value();
+  rotation = sliderR.value();
+  rotateY(rotation)
+  push();
+  noStroke();
+  translate(0,0,z)
+  texture(img)
+  plane(size);
+  pop();
+  
+  push();
+  noFill();
+  cont = 0;
+  for(x=0;x<10;x++){
+    for(y = 0;y<10;y++){
+      square((40*x)-200,(40*y)-200,40);
+    }
+  }
+  pop();
+  
+}
