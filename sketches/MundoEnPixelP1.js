0fe2b9f82e3771d94a5b73f7b0d68b86d16f6273
AndresBeta123 <AndresBeta123@users.noreply.github.com>
2022-05-21 23:25:15 +0000

    deploy: ba1b5e0d74bee9d8ef4dc490045212db885f402a

--- sketches/MundoEnPixelP1.js
+++ sketches/MundoEnPixelP1.js
@@ -17,7 +17,7 @@ function setup() {
   size = 400;
   rotation = 0;
   createCanvas(400, 400,WEBGL);
-  img = loadImage('.../tierra.jpg');
+  img = loadImage('/showcase/sketches/tierra.jpg');
 }
 
 function draw() {
