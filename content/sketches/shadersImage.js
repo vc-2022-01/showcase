531839b0d8471bd861dd92de5cef15be991dc32a
Andres <abetancurthb@unal.edu.co>
2022-06-26 17:41:58 -0500

    pruebas2

--- content/sketches/shadersImage.js
+++ content/sketches/shadersImage.js
@@ -11,7 +11,7 @@ let mask;
 
 function preload() {
 	maskShader = readShader("/showcase/sketches/mask.frag", { varyings: Tree.texcoords2,});
-  	img = loadImage("/showcase/tierra.png");
+  	img = loadImage("/showcase/sketches/tierra.jpg");
 }
 
 function setup() {
