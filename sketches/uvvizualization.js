9eb50ca7d56f4d77a71f6795b1943998f31fcfd3
jumcorrealo <jumcorrealo@users.noreply.github.com>
2022-06-29 06:19:16 +0000

    deploy: c6d3dc0fd74827a1b7f48923b4dd977ef4ef7f4a

--- sketches/uvvizualization.js
+++ sketches/uvvizualization.js
@@ -2,9 +2,9 @@ let uvShader;
 
 function preload() {
 
-  uvShader = readShader('shaders/uv.frag', { matrices: Tree.NONE, varyings: Tree.texcoords2 });
+  uvShader = readShader('/shaders/uv.frag', { matrices: Tree.NONE, varyings: Tree.texcoords2 });
 }
-
+//C:\Users\Home\Desktop\showcase\showcase\content\sketches\shaders\uv.frag
 function setup() {
   // shaders require WEBGL mode to work
   createCanvas(600, 600, WEBGL);
